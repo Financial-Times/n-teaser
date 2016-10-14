fe59178921bb4af50abcf484dec4a4888312f0d4
Ben Fletcher <bjfletcher@gmail.com>
2016-10-14 12:32:02 +0100

    add landscape template and set as a bower project (#2)

--- demos/script.js
+++ demos/script.js
@@ -1,18 +1,26 @@
-fetch('../templates/opinion.mustache')
-	.then(res => res.text())
-	.then(source => {
-	const template = Handlebars.compile(source);
-	const context = {
-		img: {
-			src: 'http://image.webservices.ft.com/v1/images/raw/fthead:mrs-moneypenny?source=origami&amp;width=60',
-			alt: 'demo image'
-		},
-		tag: 'Mrs Moneypenny',
-		heading: 'Japan sells negative yield 10-year bonds',
-		datetime: {
-			raw: '2016-02-29T12:35:48Z',
-			formatted: '2016-02-29T12:35:48Z'
-		}
-	};
-	document.querySelector('#opinion').innerHTML = template(context);
+const data = {
+	img: {
+		alt: 'demo image'
+	},
+	tag: 'Mrs Moneypenny',
+	heading: 'Japan sells negative yield 10-year bonds',
+	datetime: {
+		raw: '2016-02-29T12:35:48Z',
+		formatted: '2016-02-29T12:35:48Z'
+	}
+};
+
+const images = {
+	landscape: 'https://image.webservices.ft.com/v1/images/raw/http%3A%2F%2Fim.ft-static.com%2Fcontent%2Fimages%2Fa60ae24b-b87f-439c-bf1b-6e54946b4cf2.img?source=o-card-demo',
+	opinion: 'http://image.webservices.ft.com/v1/images/raw/fthead:mrs-moneypenny?source=origami&amp;width=60'
+};
+
+['landscape', 'opinion'].forEach(style => {
+	fetch(`../templates/${style}.mustache`)
+		.then(res => res.text())
+		.then(source => {
+		const template = Handlebars.compile(source);
+		data.img.src = images[style];
+		document.querySelector(`#${style}`).innerHTML = template(data);
+	});
 });
