d1c6ad8f7a6aaeb4846e5cd8cc150c08a9eb081e
andygnewman <andrew.newman@ft.com>
2016-11-11 08:44:36 +0000

    linting whitespace

--- src/handlebars-helpers/nTeaserPresenter.js
+++ src/handlebars-helpers/nTeaserPresenter.js
@@ -2,7 +2,7 @@
 const TeaserPresenter = require('../presenters/teaser-presenter');
 
 module.exports = (context, options) => {
-  if (options.hash) Object.assign(context, options.hash);
+	if (options.hash) Object.assign(context, options.hash);
 	if (options.data) {
 		const nTeaserPresenter = new TeaserPresenter(context);
 		return options.fn(context, {data: { nTeaserPresenter } } );
