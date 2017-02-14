b7a8fe60a2df7b6fe1dacdd071fa5b396873388d
andygnewman <andrew.newman@ft.com>
2017-02-14 11:01:49 +0000

    slight tweaking

--- tests/presenters/teaser-presenter.spec.js
+++ tests/presenters/teaser-presenter.spec.js
@@ -279,34 +279,32 @@ describe('Teaser Presenter', () => {
 
 	context('advertiserPrefix', () => {
 
-		context('has an advertiser and type of smartmatch', () => {
+		it('paid for by - when has an advertiser and type of smartmatch', () => {
 			const content = {
 				advertiser: 'Nikkei',
 				type: 'smartmatch'
 			};
-
 			subject = new Presenter(content);
 			expect(subject.advertiserPrefix).to.equal(' paid for by ');
 		});
 
-		context('has an advertiser and type of paid post', () => {
+		it('by - when has an advertiser and type of paid post', () => {
 			const content = {
 				advertiser: 'Nikkei',
 				type: 'paid-post'
 			};
-
 			subject = new Presenter(content);
 			expect(subject.advertiserPrefix).to.equal(' by ');
 		});
 
-		context('does not have an advertiser and type is article', () => {
+		it('blank - when does not have an advertiser and type is article', () => {
 			const content = {
 				type: 'article'
 			};
-
 			subject = new Presenter(content);
 			expect(subject.advertiserPrefix).to.equal('');
 		});
+
 	});
 
 	context('timeStatus', () => {
