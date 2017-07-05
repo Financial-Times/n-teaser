542651a78872ddba74076741df0bf3f5d18ec5e8
Keith Rogers <git@keithrogers.eu>
2017-07-05 09:59:13 +0100

    Apply alternative headline ability to all teasers

--- src/data-model/fragments.js
+++ src/data-model/fragments.js
@@ -16,6 +16,9 @@ module.exports = {
 			relativeUrl
 			title
 			promotionalTitle
+			alternativeTitles {
+				contentPackageTitle
+			}
 			publishedDate
 			firstPublishedDate
 			isEditorsChoice
