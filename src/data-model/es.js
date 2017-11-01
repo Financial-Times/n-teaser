5378da90ca87abc71aedc93ef6b4d3428bf060fe
Rhys Evans <rhys.evans@ft.com>
2017-11-01 15:21:41 +0000

    added es-based data model  🐿 v2.5.16

---
+++ src/data-model/es.js
@@ -0,0 +1,41 @@
+module.exports = [
+	'id',
+
+	'relativeUrl',
+	'title',
+	'alternativeTitles.promotionalTitle',
+
+	'publishedDate',
+	'firstPublishedDate',
+
+	'isEditorsChoice',
+	'isPremium',
+	'canBeSyndicated',
+
+	'mainImage.title',
+	'mainImage.description',
+	'mainImage.url',
+	'mainImage.width',
+	'mainImage.height',
+	'mainImage.ratio',
+
+	'displayConcept.id',
+	'displayConcept.prefLabel',
+	'displayConcept.relativeUrl',
+	'displayConcept.directType',
+
+	'brandConcept.id',
+	'brandConcept.prefLabel',
+	'brandConcept.relativeUrl',
+	'brandConcept.directType',
+
+	'authorConcepts.id',
+	'authorConcepts.prefLabel',
+	'authorConcepts.relativeUrl',
+	'authorConcepts.directType',
+
+	'genreConcept.id',
+	'genreConcept.prefLabel',
+	'genreConcept.relativeUrl',
+	'genreConcept.directType'
+];
